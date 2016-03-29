799d8cf3c98a626ec9be394b8803f741a067190e
Tobias Richter <tr4711@gmail.com>
2016-03-29 14:38:42 +0200

    add TaggerModel Enum

---
+++ patternmining/src/patternmining/TaggerModel.java
@@ -0,0 +1,16 @@
+package patternmining;
+
+public enum TaggerModel {
+	GERMAN("taggers/german-hgc.tagger"), ENGLISH("taggers/english-left3words-distsim.tagger");
+
+	private final String model;
+
+	TaggerModel(String model) {
+		this.model = model;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+}
