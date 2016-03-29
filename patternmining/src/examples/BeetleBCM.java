0bd9f2c5de83421b18febf8c218b9fc4f8ba1702
Tobias Richter <tr4711@gmail.com>
2016-03-29 18:10:21 +0200

    add beetle example

---
+++ patternmining/src/examples/BeetleBCM.java
@@ -0,0 +1,31 @@
+package examples;
+
+import java.util.ArrayList;
+
+import patternmining.Tagger;
+import patternmining.TaggerModel;
+
+public class BeetleBCM {
+	public static void main(String[] args) {
+		final Tagger tagger = new Tagger(TaggerModel.ENGLISH);
+
+		ArrayList<String> beetleAssociations = new ArrayList<>();
+		beetleAssociations.add("fun to drive");
+		beetleAssociations.add("neat colors");
+		beetleAssociations.add("German car");
+		beetleAssociations.add("inexpensive");
+		beetleAssociations.add("low sticker price");
+		beetleAssociations.add("good gas mileage");
+		beetleAssociations.add("good traction");
+		beetleAssociations.add("good winter car");
+		beetleAssociations.add("easy to park");
+		beetleAssociations.add("silver");
+		beetleAssociations.add("green");
+		beetleAssociations.add("30 mpg");
+		beetleAssociations.add("40 mpg");
+
+		System.out.println(tagger.tagList(beetleAssociations));
+
+	}
+
+}
