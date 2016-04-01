8b77cfadc60e2411f5f1826e08395ecaefb4e653
Tobias Richter <tr4711@gmail.com>
2016-04-01 15:48:27 +0200

    add strings as own items into itemset in sequence

--- patternmining/src/patternmining/ClosedSequentialPattern.java
+++ patternmining/src/patternmining/ClosedSequentialPattern.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 import org.slf4j.Logger;
@@ -17,25 +16,39 @@ import ca.pfv.spmf.input.sequence_database_list_strings.SequenceDatabase;
 public class ClosedSequentialPattern {
 	final static Logger LOGGER = LoggerFactory.getLogger(ClosedSequentialPattern.class);
 	final static String FILEPATH = "Bide_results.txt";
+	final static int NUMBEROFREVIEWS = 50000;
 
 	public static void main(String[] args) {
-
 		long start = System.currentTimeMillis();
-		AlgoBIDEPlus_withStrings bide = new AlgoBIDEPlus_withStrings();
-		ArrayList<String> sequenceStrings = DbDriver.getReviewPosSequences(100000);
-		SequenceDatabase sequenceDB = getSequenceDatabase(sequenceStrings);
-		LOGGER.info("{} Reviews extracted that took {} seconds", sequenceStrings.size(),
+
+		ArrayList<String> sequenceStrings = DbDriver.getReviewPosSequences(NUMBEROFREVIEWS);
+		LOGGER.info("{} Reviews extracted, that took {} seconds", sequenceStrings.size(),
 				(System.currentTimeMillis() - start) / 100);
 
+		start = System.currentTimeMillis();
+		SequenceDatabase sequenceDB = getSequenceDatabase(sequenceStrings);
+		LOGGER.info("{} Sequences put in SequenceDatabase, that took {} seconds", sequenceStrings.size(),
+				(System.currentTimeMillis() - start) / 100);
+
+		int minSupp = sequenceDB.size() / 4;
+		computeClosedSequentialPatternsBIDEPLUS(sequenceDB, minSupp);
+	}
+
+	public static void computeClosedSequentialPatternsBIDEPLUS(SequenceDatabase sequenceDB, int minSup) {
+		long start;
+		AlgoBIDEPlus_withStrings bide = new AlgoBIDEPlus_withStrings();
+
 		try {
-			bide.runAlgorithm(sequenceDB, FILEPATH, 10000);
+			start = System.currentTimeMillis();
+			bide.runAlgorithm(sequenceDB, FILEPATH, minSup);
+			LOGGER.info("{} Sequences analyzed with BIDE+, that took {} seconds \n", sequenceDB.size(),
+					(System.currentTimeMillis() - start) / 100);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+		bide.printStatistics(sequenceDB.size());
 		printResultsToConsole(FILEPATH, sequenceDB.size());
-
 	}
 
 	public static void printResultsToConsole(String path, int totalSequences) {
@@ -56,10 +69,11 @@ public class ClosedSequentialPattern {
 		}
 
 		for (String l : lines) {
-			patternToFrequency.put(l.substring(0, l.indexOf("-1") - 1),
-					Integer.parseInt(l.substring(l.lastIndexOf(" ") + 1)));
+			String pattern = "(" + l.substring(0, l.lastIndexOf("-1") - 1).replace(" -1 ", ");(") + ")";
+			patternToFrequency.put(pattern, Integer.parseInt(l.substring(l.lastIndexOf(" ") + 1)));
 
 		}
+
 		patternToFrequency = (HashMap<String, Integer>) Util.sortByValue(patternToFrequency);
 		for (String pattern : patternToFrequency.keySet()) {
 			int support = patternToFrequency.get(pattern);
@@ -76,7 +90,15 @@ public class ClosedSequentialPattern {
 		for (int i = 0; i < stringList.size(); i++) {
 			String sequenceString = stringList.get(i);
 			Sequence sequence = new Sequence(i);
-			sequence.addItemset(Arrays.asList(sequenceString.split(" ")));
+
+			// sequence.addItemset(Arrays.asList(sequenceString.split(" ")));
+
+			for (String item : sequenceString.split(" ")) {
+				ArrayList<String> itemSet = new ArrayList();
+				itemSet.add(item);
+				sequence.addItemset(itemSet);
+			}
+
 			sequenceDB.addSequence(sequence);
 		}
 		return sequenceDB;
