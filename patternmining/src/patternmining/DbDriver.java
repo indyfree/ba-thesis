523c0f4cbf0c7e0d19db07f3c470bbe5ce24d71e
Tobias Richter <tr4711@gmail.com>
2016-04-04 23:08:14 +0200

    add tokenized review extraction

--- patternmining/src/patternmining/DbDriver.java
+++ patternmining/src/patternmining/DbDriver.java
@@ -83,19 +83,35 @@ public class DbDriver {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static ArrayList<String> getReviewPosSequences(int numberReviews) {
 		ArrayList<Review> reviews = getReviews(numberReviews);
 		ArrayList<String> reviewPosSequences = new ArrayList<String>();
-		
+
 		for (Review review : reviews) {
-			if(review.getProPOS() != null) {
+			if (review.getProPOS() != null) {
 				reviewPosSequences.add(review.getProPOS());
 			}
-			if(review.getContraPOS() != null) {
+			if (review.getContraPOS() != null) {
 				reviewPosSequences.add(review.getContraPOS());
 			}
 		}
 		return reviewPosSequences;
+	}
+
+	public static ArrayList<String> getTokenizedReviewPosSequences(int numberReviews) {
+		ArrayList<String> sequences = getReviewPosSequences(numberReviews);
+		ArrayList<String> tokenizedReviews = new ArrayList<>();
+
+		for (String sequence : sequences) {
+			String[] token = sequence.split("\\$,|\\$\\.|\\$\\[");
+			for (String t : token) {
+				t = t.trim();
+				if (!t.isEmpty()) {
+					tokenizedReviews.add(t);
+				}
+			}
+		}
+		return tokenizedReviews;
 	}
 }
