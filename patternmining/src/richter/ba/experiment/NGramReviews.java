05f9394ab6c41639cb1bae3b37d9538426529560
Tobias Richter <tr4711@gmail.com>
2016-04-12 09:38:11 +0200

    remove proprocessing step

--- patternmining/src/richter/ba/experiment/NGramReviews.java
+++ patternmining/src/richter/ba/experiment/NGramReviews.java
@@ -1,23 +1,13 @@
 package richter.ba.experiment;
 
-import java.util.List;
-
 import richter.ba.db.PosSequenceRepository;
-import richter.ba.db.ReviewRepository;
 
 public class NGramReviews {
 
 	public static void main(String[] args) {
 
-		final String[] splitter = { "\\,", "\\.", "\\:", "\\;", "\\!", "\\?", "\\bund\\b", "\\boder\\b", "\\baber\\b" };
-		final String[] stopWords = { "siehe", "-", "nichts", "nix", "entfällt", "s.o." };
-		final String[] stopTags = { "$[", "XY" };
-
-		ReviewRepository reviewRep = new ReviewRepository();
-		List<String> posSequences = PreProcessor.run(reviewRep.getReviews(10000), splitter, stopWords, stopTags);
-
 		PosSequenceRepository seqRep = new PosSequenceRepository();
-		seqRep.writePosSequences(posSequences);
+
 		// NGram biGram = new NGram(2);
 		// biGram.run(posSequences, 10);
 
